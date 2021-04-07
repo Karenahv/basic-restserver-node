e84791e0b5fa9fd1041573f7e29c192b700090f9
Karen <karen@pop-os.localdomain>
2021-04-07 18:38:54 -0500

    [feature] se crea usuario en bd cuando el login es por google y el usuario no existe en bd

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,17 +52,44 @@ const login = async (req, res) => {
 
 }
 
-const googleSignIn = async (req, res)=>{
+const googleSignIn = async (req, res) => {
     const {id_token} = req.body
     try {
-         const googleUser = await googleVerify(id_token)
-        console.log(googleUser)
-        res.json({
-        msg:'TODO ok google sign in!',
-        id_token
-    })
+        const {correo, nombre, email, img} = await googleVerify(id_token)
 
-    }catch (e) {
+        // verificar si correo existe en bd
+        let usuario = await Usuario.findOne(({correo}))
+        if (!usuario) {
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+
+
+        // si el usuario en db estado false
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Generar el jwt
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            msg: 'TODO ok google sign in!',
+            usuario,
+            token,
+        })
+
+    } catch (e) {
         console.log(e)
         res.status(400).json({
             msg: 'Token de google no es válido'
