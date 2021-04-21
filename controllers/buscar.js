7c54a07ebe7400432be5826145216f3dd7ec451c
Karen <karen@pop-os.localdomain>
2021-04-20 20:53:18 -0500

    [feature] buscador usuarios, categorias y productos

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,3 +1,5 @@
+const {Producto} = require("../models");
+const {Categoria} = require("../models");
 const {Usuario} = require("../models");
 const {ObjectId} = require('mongoose').Types
 
@@ -13,12 +15,54 @@ const buscarUsuarios = async (termino='', res=response) =>{
     const esMongoId = ObjectId.isValid(termino)
     if(esMongoId){
         const usuario = await  Usuario.findById(termino);
-        res.json(usuario)
+        return res.json({
+            results: (usuario) ?[usuario]: []
+        })
     }
+
+    const regexp = new RegExp(termino, 'i')
+    const usuarios = await Usuario.find({
+        $or:[{nombre: regexp}, {correo: regexp}],
+        $and: [{estado: true}]
+    })
+    return res.json({
+            results: (usuarios) ?usuarios: []
+        })
+}
+
+const buscarCategorias = async (termino='', res=response) =>{
+    const esMongoId = ObjectId.isValid(termino)
+    if(esMongoId){
+        const categoria = await  Categoria.findById(termino);
+        return res.json({
+            results: (categoria) ?[categoria]: []
+        })
+    }
+
+    const regexp = new RegExp(termino, 'i')
+    const categorias = await Categoria.find({nombre: regexp, estado:true})
+    return res.json({
+            results: (categorias) ?categorias: []
+        })
+}
+
+const buscarProductos = async (termino='', res=response) =>{
+    const esMongoId = ObjectId.isValid(termino)
+    if(esMongoId){
+        const producto = await  Producto.findById(termino);
+        return res.json({
+            results: (producto) ?[producto]: []
+        })
+    }
+    const regexp = new RegExp(termino, 'i')
+    const producto = await Producto.find({nombre: regexp, estado:true})
+        .populate('categoria', 'nombre')
+    return res.json({
+            results: (producto) ? producto: []
+        })
 }
 
 const buscar = (req, res) => {
-
     const {coleccion, termino} = req.params
     if(!coleccionesPermitidas.includes(coleccion)){
         return res.status(400).json({
@@ -29,11 +73,13 @@ const buscar = (req, res) => {
     switch (coleccion){
         case 'usuarios':
             buscarUsuarios(termino, res)
-        break
-        case 'categoria':
-        break
+        break;
+        case 'categorias':
+            buscarCategorias(termino, res)
+        break;
         case 'productos':
-        break
+            buscarProductos(termino, res)
+        break;
 
         default:
             res.status(500).json({
